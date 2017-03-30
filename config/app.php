fc844a5d30bbabc53acca345c18360e8bae63dba
Ark <Ark@user.com>
2017-03-31 00:25:49 +0100

    Datatables installed

--- config/app.php
+++ config/app.php
@@ -176,8 +176,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-	
-	Styde\Html\HtmlServiceProvider::class
+		Styde\Html\HtmlServiceProvider::class,
+        Yajra\Datatables\DatatablesServiceProvider::class,
     ],
 
     /*
